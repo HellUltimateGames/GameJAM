dd9150e9c866c02387447e28d6532740e4f292fa
Bilihh <36853758+Bilihh@users.noreply.github.com>
2021-07-11 19:27:47 +0200

    Added Player Prefab, readded back panel for mirror in prefab and added player to room 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    public float gravity = 40.0f;
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
