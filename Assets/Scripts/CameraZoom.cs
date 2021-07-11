e62854b9df5aa10dbc724eea13abcdf481da2836
Stargheist <diegoroman2004@outlook.com>
2021-07-11 02:44:16 -0400

    added zoom for camera on right click
    added zoom functionality for the player camera that should assist with the puzzles and possibly used as the zoom feature in room 2 rather than having to aim down scopes

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public Camera playerCamera;
+    public int camFOVZoom;
+    public int camFOVNormal;
+    public int changeIncrement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().fieldOfView = camFOVNormal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire2"))
+        {
+            if (GetComponent<Camera>().fieldOfView > camFOVZoom)
+            {
+                GetComponent<Camera>().fieldOfView -= changeIncrement;
+            }
+        }
+        else
+        {
+            if(GetComponent<Camera>().fieldOfView < camFOVNormal)
+            {
+                GetComponent<Camera>().fieldOfView += changeIncrement;
+            }
+        }
+    }
+}
