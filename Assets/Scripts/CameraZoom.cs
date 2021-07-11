4d86b7fe9433bb8059da462087f152b4f47006bd
Stargheist <diegoroman2004@outlook.com>
2021-07-11 02:54:04 -0400

    player eyes removal
    removed the player eyes due to camera clipping issues

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraZoom : MonoBehaviour
-{
-    
-    Camera playerCamera;
-    
+{ 
     
     public int camFOVZoom;
     public int camFOVNormal;
