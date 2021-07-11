1d3f52ed8dfbc92acf8736f3bb7ca6a8b4508dd8
Bilihh <36853758+Bilihh@users.noreply.github.com>
2021-07-11 21:07:58 +0200

    Update PictureFrame.cs

--- Assets/Scripts/PictureFrame.cs
+++ Assets/Scripts/PictureFrame.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PictureFram : MonoBehaviour
+public class PictureFrame : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
